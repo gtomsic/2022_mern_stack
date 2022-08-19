64fac888ab5bdee7e76deba1358daa35403fdb32
Gabriel Tomsic <gabriel@macmax.local>
2022-08-18 20:34:51 -0600

    Adding auth middle to all goals routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -5,9 +5,11 @@ const {
   deleteGoal,
 } = require("../controllers/goalController");
 
+const { auth } = require("../middlewares/authMiddleware");
+
 const router = require("express").Router();
 
-router.route("/").get(getGoals).post(setGoal);
-router.route("/:id").put(updateGoal).delete(deleteGoal);
+router.route("/").get(auth, getGoals).post(auth, setGoal);
+router.route("/:id").put(auth, updateGoal).delete(auth, deleteGoal);
 
 module.exports = router;
